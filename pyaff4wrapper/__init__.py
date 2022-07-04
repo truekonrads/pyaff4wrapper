60fb7b45cc02769c4e784e4f81349805fd4817af
Konrads Klints <kklints@kpmg.com.sg>
2022-07-04 14:41:45 +0800

    migrate to flit backend

--- pyaff4wrapper/__init__.py
+++ pyaff4wrapper/__init__.py
@@ -1,3 +1,4 @@
-from .wrapper import Aff4Wrapper
 """A ZipFile like package for extracting data from Aff4 packages"""
+from .wrapper import Aff4Wrapper
 __all__ = ['Aff4Wrapper']
+__version__ = "0.1"
